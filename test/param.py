c781089bb09efb3e960e9e505326a206023b51d2
TVTyuVEN <862054822@qq.com>
2020-03-16 15:58:11 +0800

    update

--- test/param.py
+++
@@ -1,2 +0,0 @@
-debug_postman = 1
-port_id = 5000
--- test/run.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask import jsonify
-from flask_cors import CORS
-from apps import register_blueprints
-from apps.util import init_logger
-from apps.errcode import ErrCode
-from apps.v1_0.test import *
-
